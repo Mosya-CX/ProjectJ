81b328269e63c65778a499ace3543b1023ed1df3
Mosya-CX <1906848521@qq.com>
2024-03-01 22:20:53 +0800

    1

---
+++ Assets/Resources/Script/Item/BaseItem.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseItem 
+{
+    // »ù´¡±äÁ¿
+    public bool isUsed = false;
+
+    public virtual void OnCreate()
+    {
+
+    }
+
+    public virtual void OnUpdate()
+    {
+
+    }
+
+    public virtual void OnDestory()
+    {
+
+    }
+
+    public virtual void Effect()
+    {
+
+    }
+}
---
+++ Assets/Resources/Script/Item/Item01.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item01 : BaseItem
+{
+    public override void OnCreate()
+    {
+        base.OnCreate();
+    }
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+    }
+
+    public override void OnDestory()
+    {
+        base.OnDestory();
+    }
+
+    public override void Effect()
+    {
+        base.Effect();
+    }
+}
