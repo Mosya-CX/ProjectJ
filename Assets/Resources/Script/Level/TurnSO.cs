f09db8e901fa851c5d00b8b487622fdcf03fe15f
Mosya-CX <1906848521@qq.com>
2024-03-27 14:41:00 +0800

    1

--- Assets/Resources/Script/Level/TurnSO.cs
+++ Assets/Resources/Script/Level/TurnSO.cs
@@ -7,13 +7,17 @@ using UnityEngine;
 public class TurnData : ScriptableObject
 {
     public GameObject TurnScene;// 场景
-    public Vector3 BornPos;// 初始位置
+    public Vector3 BornPos;// 玩家初始位置
     public PlayerController playerData;// 记录玩家信息
-    public Vector2 ScenePos;
-    public virtual void OnCreate()
+    public Transform parent;
+    public bool isOver;// 判断是否结束
+
+    public virtual GameObject OnCreate()
     {
         // 重置玩家位置
         playerData.transform.position = BornPos;
+
+        GameObject scene;
         // 加载场景
         if (!LevelManager.Instance.loadedScene.ContainsKey(TurnScene.name))
         {
@@ -21,18 +25,20 @@ public class TurnData : ScriptableObject
             {
                 LevelManager.Instance.curScene.SetActive(false);
             }
-            GameObject scene = Instantiate(TurnScene, ScenePos, Quaternion.identity);// 生成到场景上
+            scene = Instantiate(TurnScene, Vector3.zero, Quaternion.identity);// 生成到场景上
+            scene.transform.SetParent(parent, false);
             scene.name = TurnScene.name;
-            LevelManager.Instance.curScene = scene;
+            //LevelManager.Instance.curScene = scene;
         }
         else
         {
-            GameObject scene = LevelManager.Instance.loadedScene[TurnScene.name];// 从字典中获取
+            scene = LevelManager.Instance.loadedScene[TurnScene.name];// 从字典中获取
             scene.SetActive(true);// 激活
-            LevelManager.Instance.curScene = scene;
+            //LevelManager.Instance.curScene = scene;
+            
         }
-
-
+        isOver = false;
+        return scene;
     }
     public virtual void OnUpdate()
     {
@@ -42,7 +48,7 @@ public class TurnData : ScriptableObject
     {
         LevelManager.Instance.curTurn++;
         playerData.playerState = PlayerState.None;
-
+        
         LevelManager.Instance.LoadTurn();// 加载下一个Turn
     }
 
