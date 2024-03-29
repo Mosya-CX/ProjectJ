578a84d925f0349893d26a189ec132671d45da22
Heimaofd <142678904+Heimaofd@users.noreply.github.com>
2024-03-29 17:09:03 +0800

    1

--- Assets/Resources/Script/CameraControl.cs
+++ Assets/Resources/Script/CameraControl.cs
@@ -27,13 +27,13 @@ public class CameraControl : MonoBehaviour
         {
             if (transform.position != target.transform.position)
             {
-                //对x和y坐标进行限制
-                //float x = Mathf.Clamp(target.transform.position.x, -curScene.GetComponent<SpriteRenderer>().sprite.bounds.size.x/2 + GameManager.Instance.viewWidth / 2,/*地图左边缘x加上一半摄像机x*/
-                    //curScene.GetComponent<SpriteRenderer>().sprite.bounds.size.x / 2 - GameManager.Instance.viewWidth / 2/*地图右边缘x减去一半摄像机x*/);
-               // float y = Mathf.Clamp(target.transform.position.y, -curScene.GetComponent<SpriteRenderer>().bounds.size.x / 2 + GameManager.Instance.viewHeight / 2,/*地图下边缘y加上一半摄像机y*/
-                     // curScene.GetComponent<SpriteRenderer>().sprite.bounds.size.y/2 - GameManager.Instance.viewHeight / 2/*地图上边缘y加去一半摄像机y */);
+               //对x和y坐标进行限制
+                float x = Mathf.Clamp(target.transform.position.x, -curScene.GetComponent<SpriteRenderer>().sprite.bounds.size.x/2 + GameManager.Instance.viewWidth / 2,/*地图左边缘x加上一半摄像机x*/
+                    curScene.GetComponent<SpriteRenderer>().sprite.bounds.size.x / 2 - GameManager.Instance.viewWidth / 2/*地图右边缘x减去一半摄像机x*/);
+                float y = Mathf.Clamp(target.transform.position.y, -curScene.GetComponent<SpriteRenderer>().bounds.size.x / 2 + GameManager.Instance.viewHeight / 2,/*地图下边缘y加上一半摄像机y*/
+                      curScene.GetComponent<SpriteRenderer>().sprite.bounds.size.y / 2 - GameManager.Instance.viewHeight / 2/*地图上边缘y加去一半摄像机y */);
                 //再进行lerp
-               // transform.position = Vector2.Lerp(transform.position, new Vector3(x, y, -10), smooth);
+                transform.position = Vector2.Lerp(transform.position, new Vector3(x, y, -10), smooth);
             }
         }
         if (target == null&&curScene!=null )//移动到某个指定点
@@ -42,12 +42,12 @@ public class CameraControl : MonoBehaviour
             if (transform.position != Position)
             {
                 //对x和y坐标进行限制
-               // float x = Mathf.Clamp(target.transform.position.x, -curScene.GetComponent<SpriteRenderer>().sprite.bounds.size.x / 2 + GameManager.Instance.viewWidth / 2,/*地图左边缘x加上一半摄像机x*/
-                  // curScene.GetComponent<SpriteRenderer>().sprite.bounds.size.x / 2 - GameManager.Instance.viewWidth / 2/*地图右边缘x减去一半摄像机x*/);
-               // float y = Mathf.Clamp(target.transform.position.y, -curScene.GetComponent<SpriteRenderer>().bounds.size.x / 2 + GameManager.Instance.viewHeight / 2,/*地图下边缘y加上一半摄像机y*/
-                     // curScene.GetComponent<SpriteRenderer>().sprite.bounds.size.y / 2 - GameManager.Instance.viewHeight / 2/*地图上边缘y加去一半摄像机y */);
+                float x = Mathf.Clamp(target.transform.position.x, -curScene.GetComponent<SpriteRenderer>().sprite.bounds.size.x / 2 + GameManager.Instance.viewWidth / 2,/*地图左边缘x加上一半摄像机x*/
+                  curScene.GetComponent<SpriteRenderer>().sprite.bounds.size.x / 2 - GameManager.Instance.viewWidth / 2/*地图右边缘x减去一半摄像机x*/);
+                float y = Mathf.Clamp(target.transform.position.y, -curScene.GetComponent<SpriteRenderer>().bounds.size.x / 2 + GameManager.Instance.viewHeight / 2,/*地图下边缘y加上一半摄像机y*/
+                      curScene.GetComponent<SpriteRenderer>().sprite.bounds.size.y / 2 - GameManager.Instance.viewHeight / 2/*地图上边缘y加去一半摄像机y */);
                 //再进行lerp
-                //transform.position = Vector2.Lerp(transform.position, new Vector3(x, y, -10), smooth);
+                transform.position = Vector2.Lerp(transform.position, new Vector3(x, y, -10), smooth);
             }
         }
     }
@@ -78,6 +78,7 @@ public class CameraControl : MonoBehaviour
     }
     public void GetCursceneAndEnable(GameObject Curscene)//获取curscene，并且启动摄像机跟随
     {
+        print(Curscene);
         curScene = Curscene;
     }
     public void DeleteCursceneAndStop()//关闭摄像机跟随
