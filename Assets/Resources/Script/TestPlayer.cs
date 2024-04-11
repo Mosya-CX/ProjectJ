4c5630b67981706c507bf11861297777d50e5073
Heimaofd <142678904+Heimaofd@users.noreply.github.com>
2024-04-11 22:50:51 +0800

    1

--- Assets/Resources/Script/TestPlayer.cs
+++ Assets/Resources/Script/TestPlayer.cs
@@ -17,7 +17,7 @@ public class TestPlayer : MonoBehaviour
         if (UnityEngine.Input.GetMouseButtonDown(0))
         {
            TimeManager.TimeOut(true);//combo导致的暂停\
-            ComboManager.AddComboNum(1);//攻击成功
+            ComboManager.Instance.AddComboNum(1);//攻击成功
             soundManager.Instance.Playsfx("b");
             print("dkjasjfkdl");
         }
